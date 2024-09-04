33f5a341787bee41ce2dc09c7c10354d49a5f915
parkk129 <seohyun3517@naver.com>
2024-09-04 18:02:39 +0900

    0904

---
+++ cra/link-app/src/index.js
@@ -0,0 +1,7 @@
+import App from './App';
+import ReactDOM from "react-dom/client"
+
+const container = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById(container));
+
+root.render(<App />);
