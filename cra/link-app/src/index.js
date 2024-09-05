90263b87c38c542a78448d54bce596eb18463079
parkk129 <seohyun3517@naver.com>
2024-09-05 18:02:25 +0900

    0905

--- cra/link-app/src/index.js
+++ cra/link-app/src/index.js
@@ -1,7 +1,25 @@
-import App from './App';
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
-const container = ReactDOM.createRoot(document.getElementById('root'));
-const root = ReactDOM.createRoot(document.getElementById(container));
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+}
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
